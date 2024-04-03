7f378474f513f40450d833115c92de9a946adcaa
Matthew Frazier <55844662+mcfrazier@users.noreply.github.com>
2024-04-02 22:39:09 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ configs/win/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Set-Alias -Name vi -Value 'C:\Program Files\Vim\vim91\vim.exe'
+Set-Alias -Name vim -Value 'C:\Program Files\Vim\vim91\vim.exe'
+
+Set-PSReadLineOption -EditMode vi -BellStyle None
+
+$vimrc="~\_vimrc"
